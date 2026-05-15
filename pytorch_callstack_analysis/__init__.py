8cffbd9c8e69b6e98dbeaad6ecb7cd0ad9e15391
yongpan.hua <yongpan.hua@mthreads.com>
2026-05-15 23:01:50 +0800

    Add callstack analysis package entrypoint

---
+++ pytorch_callstack_analysis/__init__.py
@@ -0,0 +1,11 @@
+from pytorch_callstack_analysis.call_graph_template import (
+    extract_dup_or_shape_func_name_from_template,
+    extract_func_name_from_template,
+    set_pandas_display_options,
+)
+
+__all__ = [
+    "extract_dup_or_shape_func_name_from_template",
+    "extract_func_name_from_template",
+    "set_pandas_display_options",
+]
