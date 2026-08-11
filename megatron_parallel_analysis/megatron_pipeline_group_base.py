c2866b9b3892d9e817f8dda5de0f4afacad1a22c
yongpan.hua <yongpan.hua@mthreads.com>
2026-08-11 13:00:27 +0800

    Reduce PP group parsing memory use
    Avoid copying the full rank DataFrame before filtering down to user annotations.

--- megatron_parallel_analysis/megatron_pipeline_group_base.py
+++ megatron_parallel_analysis/megatron_pipeline_group_base.py
@@ -36,20 +36,24 @@ def parallel_callgraph_create(rank_id, trace_file, bwd_annotation_str='backward_
     t.decode_symbol_ids(use_shorten_name=False)
     cg = CallGraph(t, bwd_annotation_str=bwd_annotation_str)
     _, main_stack = get_main_stack_on_rank(cg, rank_id)
-    full_df = main_stack.full_df.copy()
-    del cg
-    del t
+    full_df = main_stack.full_df
     #Todo: ProfilerStep col kernel span has a wrong value
     profiler_step_mask = full_df['s_name'].str.contains(r'^ProfilerStep#.*')
     pretrain_mask = full_df['s_name'].str.match(
         pat=r"^megatron/training/training.py\(\d+\): pretrain$"
     )
     profiler_step_count = int(profiler_step_mask.sum())
-    pretrain_rows = full_df.loc[pretrain_mask, ['s_name', 'ts', 'dur', 'kernel_span']]
-    pretrain_count = len(pretrain_rows)
+    pretrain_kernel_span = full_df.loc[pretrain_mask, 'kernel_span'].to_numpy()
+    pretrain_count = len(pretrain_kernel_span)
+    result_df = full_df.loc[full_df['s_cat'] == 'user_annotation'].copy()
+    del main_stack
+    del cg
+    del t
+    del full_df
     if profiler_step_count > 0:
         if profiler_step_count == pretrain_count:
-            full_df.loc[profiler_step_mask, 'kernel_span'] = pretrain_rows['kernel_span'].to_numpy()
+            result_profiler_step_mask = result_df['s_name'].str.contains(r'^ProfilerStep#.*')
+            result_df.loc[result_profiler_step_mask, 'kernel_span'] = pretrain_kernel_span
         elif pretrain_count == 0:
             # Only user annotation, but set PROFILER_WITH_STACK=0
             logger.warning(
@@ -61,7 +65,7 @@ def parallel_callgraph_create(rank_id, trace_file, bwd_annotation_str='backward_
             )
     else:
         raise RuntimeError('ProfilerStep annotations not found in trace, cannot override kernel_span values')
-    return rank_id, full_df[full_df['s_cat'] == 'user_annotation' ]
+    return rank_id, result_df
 
 # Todo: Does the pp group trace class need to know information about other tp, ep, dp groups?
 class MegatronPipelineParallelGroupTraceBase(ABC):
